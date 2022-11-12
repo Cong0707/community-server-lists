c875635f29d5d688c497bcc6f7233b32a188ebbb
Cong0707 <49234756+Cong0707@users.noreply.github.com>
2022-11-12 12:04:39 +0800

    main module
    second

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+Events.on(EventType.ClientLoadEvent, () => {
+    var url = "https://ghproxy.com/github.com/Anuken/Mindustry/blob/master/servers_v7.json"
+    var callback = d => {
+        try {
+            let list = JSON.parse(new java.lang.String(d.getResult(), "UTF-8"));
+            Core.app.post(() => {
+                try {
+                    //Vars.defaultServers.clear();
+                    list.forEach(t => {
+                        Vars.defaultServers.add(ServerGroup(t.name, t.address))
+                    })
+                } catch (error) { print(error) }
+            })
+        } catch (error) { print(error) }
+    }
+
+    if (Version.build >= 136)
+        Http.get(url, callback)
+    else
+        Core.net.httpGet(url, callback, () => { })
+})
